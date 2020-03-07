75bf12faed22d4f938d903c646f3fdb2af2fc745
monyarm <monyarm@gmail.com>
2020-03-07 03:40:27 +0200

    CharacterController

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController cController;
+
+    private float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    private bool jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+         if (Input.GetButtonDown("Jump"))
+         {
+             jump = true;
+         }
+    }
+
+    private void FixedUpdate()
+    {
+        cController.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
